659511c627584b61df74b60a2105ba039684addd
sylabtv <sylab.studio@gmail.com>
2026-01-14 14:50:16 +0100

    transformer un objet en JSON﻿

---
+++ exercice-3/index.js
@@ -0,0 +1,17 @@
+let data = {
+    firstName : "Harry",
+    lastName : "Bow",
+    motto : "C'est beau la vie !"
+};
+
+let str = "{\"firstName\":\"Colonel\",\"lastName\":\"Sanders\",\"motto\":\"Il est bon mon poulet\"}";
+
+document.addEventListener("DOMContentLoaded", function(){
+let json = JSON.stringify(data);
+console.log(json);
+
+let obj = JSON.parse(str);
+console.log(obj);
+});
+
+
