f5d7214c011dcb89f996abb1cc5d6bbcc772abd3
sylabtv <sylab.studio@gmail.com>
2026-01-15 12:00:15 +0100

    récupérer les données d'un utilisateur﻿

---
+++ Fetch/exercice-1 copy/index.js
@@ -0,0 +1,19 @@
+// declarer l'url api
+let url = "https://corsproxy.io/?" + encodeURIComponent(
+    "https://maridoucet.sites.3wa.io/user-api/user/15"
+);
+document.addEventListener("DOMContentLoaded", function() {
+// appeler l'url avec fetch
+    fetch(url, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json"
+        }
+    })
+// transformer en obj (response.json equivaut à JSON.parse)
+    .then(response => response.json())
+// afficher dans la console 
+    .then(data => console.log(data))
+// le reject renvoi une erreur
+    .catch(error => console.error(error));
+});
