4567d414e5f6300ad42efb4b54a5a41b9192778c
sylabtv <sylab.studio@gmail.com>
2026-01-15 15:33:04 +0100

    envoyer des informations﻿

--- Fetch/exercice-3/index.js
+++ Fetch/exercice-3/index.js
@@ -1,16 +1,20 @@
 // declarer l'url api
 let url = "https://corsproxy.io/?" + encodeURIComponent(
-    "https://maridoucet.sites.3wa.io/user-api/users"
+    "https://maridoucet.sites.3wa.io/user-api/create-user"
 );
+let formData = new FormData();
+formData.append('username', "Sylab");
+formData.append('firstName', "Oh puree");
+formData.append('lastName', "el goat");
+formData.append('email', "shirrabu@3wa.com");
 
+let body = {
+    method: 'POST',
+    body: formData
+};
 document.addEventListener("DOMContentLoaded", function() {
 // appeler l'url avec fetch
-    fetch(url, {
-        method: "GET",
-        headers: {
-            "Content-Type": "application/json"
-        }
-    })
+    fetch(url,body)
 // transformer en obj (response.json equivaut à JSON.parse)
     .then(response => response.json())
 // afficher dans la console 
