8aeefd0426fa6b76e768dc78c015435bd832e697
sylabtv <sylab.studio@gmail.com>
2026-01-15 12:13:22 +0100

    récupérer un tableau de données utilisateurs﻿

--- Fetch/exercice-2/index.js
+++ Fetch/exercice-2/index.js
@@ -1,6 +1,6 @@
 // declarer l'url api
 let url = "https://corsproxy.io/?" + encodeURIComponent(
-    "https://maridoucet.sites.3wa.io/user-api/user/15"
+    "https://maridoucet.sites.3wa.io/user-api/users"
 );
 document.addEventListener("DOMContentLoaded", function() {
 // appeler l'url avec fetch
