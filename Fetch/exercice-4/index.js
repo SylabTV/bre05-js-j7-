f1042c8b7a63c2edac4ad93ab290b30d37a44970
sylabtv <sylab.studio@gmail.com>
2026-01-16 10:36:38 +0100

    générer du HTML avec les infos récupérées avec fetch﻿

--- Fetch/exercice-4/index.js
+++ Fetch/exercice-4/index.js
@@ -1,41 +1,29 @@
-// declarer l'url api pour recuperer tous les utilisateurs
+// declarer l'url api
 let url = "https://corsproxy.io/?" + encodeURIComponent(
-    "https://maridoucet.sites.3wa.io/user-api/users"
+    "https://maridoucet.sites.3wa.io/user-api/user/18"
 );
-
 document.addEventListener("DOMContentLoaded", function() {
-
-    // appeler l'url avec fetch
+// appeler l'url avec fetch
     fetch(url)
-        // transformer la reponse en tableau JS
-        .then(response => response.json())
-        .then(data => {
-            // creer le tableau avec le thead
-            // utiliser map pour generer automatiquement une ligne <tr> par utilisateur
-            let tableHTML = `
-                <table border="1">
-                    <thead>
-                        <tr>
-                            <th>Username</th>
-                            <th>Nom complet</th>
-                            <th>Email</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        ${data.map(user => `
-                            <tr>
-                                <td>${user.username}</td>
-                                <td>${user.firstName} ${user.lastName}</td>
-                                <td>${user.email}</td>
-                            </tr>
-                        `).join('')}
-                    </tbody>
-                </table>
-            `;
+// transformer en obj (response.json equivaut à JSON.parse)
+    .then(response => response.json())
+// cree les elements et remplir avec les datas
+    .then(data => {
+        let user = data.data;
 
-            // ajouter le tableau dans le body du document
-            document.body.innerHTML += tableHTML;
-        })
-        // si une erreur arrive
-        .catch(error => console.error(error));
+        let h1 = document.createElement("h1");
+        let h2 = document.createElement("h2");
+        let h3 = document.createElement("h3");
+
+        h1.textContent = user.username;
+        h2.textContent = user.firstName + " " + user.lastName;
+        h3.textContent = user.email;
+// ajouter les elements dans le dom 
+    document.body.appendChild(h1);
+    document.body.appendChild(h2);
+    document.body.appendChild(h3);
+    console.log(data);
+    })
+// le reject renvoi une erreur
+    .catch(error => console.error(error));
 });
